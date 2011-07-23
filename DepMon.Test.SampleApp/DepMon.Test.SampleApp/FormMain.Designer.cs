cf6e259ada99adb3465e1658713b771bcd4c3a7d
nico <nico@localhost>
2011-07-23 22:17:35 +0200

    added sample app

---
+++ DepMon.Test.SampleApp/DepMon.Test.SampleApp/FormMain.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace DepMon.Test.SampleApp
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAddProvider = new System.Windows.Forms.Button();
+            this.comboBoxProviders = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonAddProvider
+            // 
+            this.buttonAddProvider.Location = new System.Drawing.Point(199, 12);
+            this.buttonAddProvider.Name = "buttonAddProvider";
+            this.buttonAddProvider.Size = new System.Drawing.Size(28, 23);
+            this.buttonAddProvider.TabIndex = 5;
+            this.buttonAddProvider.Text = "...";
+            this.buttonAddProvider.UseVisualStyleBackColor = true;
+            this.buttonAddProvider.Click += new System.EventHandler(this.buttonAddProvider_Click);
+            // 
+            // comboBoxProviders
+            // 
+            this.comboBoxProviders.FormattingEnabled = true;
+            this.comboBoxProviders.Location = new System.Drawing.Point(65, 14);
+            this.comboBoxProviders.Name = "comboBoxProviders";
+            this.comboBoxProviders.Size = new System.Drawing.Size(128, 21);
+            this.comboBoxProviders.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Provider:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 384);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAddProvider);
+            this.Controls.Add(this.comboBoxProviders);
+            this.Name = "FormMain";
+            this.Text = "DepMon Sample Application";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAddProvider;
+        private System.Windows.Forms.ComboBox comboBoxProviders;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
